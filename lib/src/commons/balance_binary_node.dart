d0c5053e92da5db995f1e38dc53460e26f6a6342
Dmitry Elagin <dmitry.elagin@team.wrike.com>
2021-06-29 20:09:12 +0300

    Move work with parent to different node class

--- lib/src/commons/balance_binary_node.dart
+++ lib/src/commons/balance_binary_node.dart
@@ -1,40 +1,31 @@
 import 'dart:math';
 
-import 'binary_node.dart';
+import 'linked_binary_node.dart';
 
 class BalanceBinaryNode<T, N extends BalanceBinaryNode<T, N>>
-    extends BinaryNode<T, N> {
+    extends LinkedBinaryNode<T, N> {
   BalanceBinaryNode(T value, [N? left, N? right]) : super(value, left, right);
-
-  N? _left;
-  N? _right;
-  N? _parent;
 
   int _height = 0;
 
   @override
-  N? get left => _left;
+  N? get left => super.left;
 
   @override
   set left(N? node) {
-    if (_left?._parent == this) _left!._parent = null;
-    node?._changeParent(this as N);
-    _left = node;
+    super.left = node;
     _alignHeight();
   }
 
   @override
-  N? get right => _right;
+  N? get right => super.right;
 
   @override
   set right(N? node) {
-    if (_right?._parent == this) _right!._parent = null;
-    node?._changeParent(this as N);
-    _right = node;
+    super.right = node;
     _alignHeight();
   }
 
-  N? get parent => _parent;
   N? get tallestChild => _heightDifference < 0 ? right : left;
 
   bool get isBalanced => _heightDifference.abs() <= 1;
@@ -42,17 +33,11 @@ class BalanceBinaryNode<T, N extends BalanceBinaryNode<T, N>>
 
   int get _heightDifference => (left?._height ?? -1) - (right?._height ?? -1);
 
-  void _changeParent(N? parent) {
-    if (_parent?.left == this) _parent!.left = null;
-    if (_parent?.right == this) _parent!.right = null;
-    _parent = parent;
-  }
-
   void _alignHeight() {
     final height = _computeHeight();
     final isChangedHeight = _height != height;
     _height = height;
-    if (isChangedHeight) _parent?._alignHeight();
+    if (isChangedHeight) parent?._alignHeight();
   }
 
   int _computeHeight() {
---
+++ lib/src/commons/linked_binary_node.dart
@@ -0,0 +1,38 @@
+import 'binary_node.dart';
+
+class LinkedBinaryNode<T, N extends LinkedBinaryNode<T, N>>
+    extends BinaryNode<T, N> {
+  LinkedBinaryNode(T value, [N? left, N? right]) : super(value, left, right);
+
+  N? _left;
+  N? _right;
+  N? _parent;
+
+  @override
+  N? get left => _left;
+
+  @override
+  set left(N? node) {
+    if (_left?._parent == this) _left!._parent = null;
+    node?._changeParent(this as N);
+    _left = node;
+  }
+
+  @override
+  N? get right => _right;
+
+  @override
+  set right(N? node) {
+    if (_right?._parent == this) _right!._parent = null;
+    node?._changeParent(this as N);
+    _right = node;
+  }
+
+  N? get parent => _parent;
+
+  void _changeParent(N? parent) {
+    if (_parent?.left == this) _parent!.left = null;
+    if (_parent?.right == this) _parent!.right = null;
+    _parent = parent;
+  }
+}
