63960e3376cce119a8ba27c88da17d1947cabc16
Dmitry Elagin <dmitry.elagin@team.wrike.com>
2021-07-02 21:42:34 +0300

    Improve AVL tree stability

--- lib/src/trees/avl_tree.dart
+++ lib/src/trees/avl_tree.dart
@@ -33,16 +33,12 @@ class AVLTree<T> extends BaseBinarySearchTree<T, _BinaryNode<T>> {
   void _rebalance(_BinaryNode<T>? z) {
     if (z == null || z.isBalanced) return;
     final y = z.tallestChild!, x = y.tallestChild!;
-    if (y.isLeftOf(z) && x.isLeftOf(y)) z.rotateRight();
-    if (y.isRightOf(z) && x.isRightOf(y)) z.rotateLeft();
-    if (y.isLeftOf(z) && x.isRightOf(y)) {
-      y.rotateLeft();
-      z.rotateRight();
-    }
-    if (y.isRightOf(z) && x.isLeftOf(y)) {
-      y.rotateRight();
-      z.rotateLeft();
-    }
+    final isLeftYZ = y.isLeftOf(z), isRightYZ = y.isRightOf(z);
+    final isLeftXY = x.isLeftOf(y), isRightXY = x.isRightOf(y);
+    if (isLeftYZ && isRightXY) y.rotateLeft();
+    if (isRightYZ && isLeftXY) y.rotateRight();
+    if (isLeftYZ) z.rotateRight();
+    if (isRightYZ) z.rotateLeft();
     if (y.hasNoParent) root = y;
     if (x.hasNoParent) root = x;
   }
